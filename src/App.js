2f556a00077ee3b8137b09075c36fae90e7dedaf
eugene shuliakivsky <zimishe@gmail.com>
2017-02-22 17:57:53 +0200

    gmap react initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Map from './components/map'
+
+class App extends Component {
+    render() {
+        return(
+            <div className="gmap-app">
+                <Map />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/map.js
@@ -0,0 +1,19 @@
+/**
+ * Created by eugene on 22.02.17.
+ */
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+class Map extends Component {
+    render() {
+        const KEY = 'AIzaSyDRUCOhK3QDvocwOtZMG4_Eyaw6dBbm95A';
+        
+        return (
+            <div id="map"></div>
+        )
+    }
+}
+
+export default Map
