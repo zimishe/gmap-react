22667882a6f10f575cd89aa62469de708171cfd5
eugene shuliakivsky <zimishe@gmail.com>
2017-02-27 17:49:15 +0200

    deploy

--- src/actions/support/handleErrors.js
+++ src/actions/support/handleErrors.js
@@ -3,8 +3,5 @@
  */
 
 export function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
+    
 }
